d44c2c6010bff5f133b4fea53f087eebd09a5080
Delfin656 <juandelfin656@gmail.com>
2022-10-25 07:48:32 +0000

    First commit actualizando estilos en css en el boton close y margenes colores etc

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+//import { Input } from "./Todo.jsx"
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import { Task } from "./Task.jsx";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const home = () => {
+  const [newTask, setNewTask] = useState({ label: "", done: false });
+  const [todo, setTodo] = useState([]);
+  const handleChange = (event) => {
+    setNewTask({
+      ...newTask,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const addTask = (event) => {
+    if (event.key == "Enter") {
+      if (newTask.label.trim() != "") {
+        setTodo([...todo, newTask]);
+        setNewTask({ label: "", done: false });
+        console.log("debo guardar la tarea");
+      } else {
+        console.log("no se aceptan cambios vacios");
+      }
+
+      // console.log("presiona enter")
+    }
+  };
+  const deleteTask = (id) => {
+    const newListTask = todo.filter((task, index) => id != index);
+    setTodo(newListTask);
+  };
+  return (
+    <div className="container">
+      <div className="">
+        <h1 className="text-center">ToDos</h1>
+      </div>
+      <div className="">
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            value={newTask.label}
+            name={"label"}
+            placeholder="Agregar tareas"
+            onChange={handleChange}
+            onKeyDown={addTask}
+          />
+        </div>
+      </div>
+      <div>
+        <ul>
+          {todo.map((task, index) => {
+            return (
+              <li key={index}>
+                <button
+                  type="button"
+                  class="btn-close btn-close-white"
+                  aria-label="Close"
+                  onClick={() => deleteTask(index)}
+                ></button>
+                {task.label}
+              </li>
+            );
+          })}
+        </ul>
+		
+        {todo.length + " tareas"}
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default home;
